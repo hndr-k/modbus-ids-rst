692df7d45fdbb32216b9296176d6bb33d3a73e66
Hendrik Nessau <hndrknessi@gmail.com>
2025-10-21 11:31:15 +0200

    change pymodbus version to 3.6.6 and change server to ranomize a temperature reading

--- modbus-server/server/modbus-server.py
+++ modbus-server/server/modbus-server.py
@@ -1,28 +1,31 @@
+import random
 import logging
-from pymodbus.server import StartTcpServer
-from pymodbus.datastore import (
-    ModbusSequentialDataBlock,
-    ModbusDeviceContext,
-    ModbusServerContext,
+from pymodbus.datastore import ModbusSequentialDataBlock, ModbusSlaveContext, ModbusServerContext
+from pymodbus.server.async_io import StartTcpServer
+
+logging.basicConfig()
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
+
+coils = ModbusSequentialDataBlock(1, [False] * 100)
+discrete_inputs = ModbusSequentialDataBlock(1, [False] * 100)
+h_reg = ModbusSequentialDataBlock(1, [0] * 100)
+in_reg = ModbusSequentialDataBlock(1, [0] * 100)
+
+temperature_values = [random.randint(4, 15) for _ in range(7)]
+h_reg.setValues(1, temperature_values)
+print("temperature_values:", temperature_values)
+
+
+slave_context = ModbusSlaveContext(
+    di=discrete_inputs,
+    co=coils,
+    hr=h_reg,
+    ir=in_reg
 )
 
-logging.basicConfig(level=logging.INFO)
 
-def run():
+server_context = ModbusServerContext(slaves=slave_context, single=True)
 
-    device = ModbusDeviceContext(
-        di=ModbusSequentialDataBlock(0, [0] * 100),  # discrete
-        co=ModbusSequentialDataBlock(0, [0] * 100),  # coils
-        ir=ModbusSequentialDataBlock(0, [0] * 100),  # input reg
-        hr=ModbusSequentialDataBlock(0, [0] * 100),  # holding reg
-    )
 
-    #
-    context = ModbusServerContext(devices=device, single=True)
-
-    host, port = "0.0.0.0", 5020  
-    logging.info("Starting Modbus TCP server on %s:%s ...", host, port)
-    StartTcpServer(context, address=(host, port))
-
-if __name__ == "__main__":
-    run()
+StartTcpServer(context=server_context, address=("0.0.0.0", 5020))
