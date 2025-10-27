2f8b6b1b04480730140d3d5ca8f597b3b64ba749
Hendrik Nessau <hndrknessi@gmail.com>
2025-10-27 10:36:04 +0100

    keep the client alive

--- modbus-traffic-gen/modbus-traffic-gen/traffic_gen.py
+++ modbus-traffic-gen/modbus-traffic-gen/traffic_gen.py
@@ -1,23 +1,35 @@
+import time
 from pymodbus.client.tcp import ModbusTcpClient
 
 client = ModbusTcpClient("modbus-server", port=5020)
 
+if not client.connect():
+    raise Exception("Unable to connect to Modbus server")
 
-coils = client.read_coils(address=0, count=100)
-discrete_inputs = client.read_discrete_inputs(address=0, count=100)
-holding_registers = client.read_holding_registers(address=0, count=100)
-input_registers = client.read_input_registers(address=0, count=100)
+try:
+    while True:
+        coils = client.read_coils(address=0, count=100)
+        discrete_inputs = client.read_discrete_inputs(address=0, count=100)
+        holding_registers = client.read_holding_registers(address=0, count=100)
+        input_registers = client.read_input_registers(address=0, count=100)
 
+        if coils.isError():
+            print(f"Error getting coils: {coils}")
+        else:
+            print("Coils:", coils.bits)
 
-if coils.isError():
-    print('Error getting coils: {coils}')
-    raise Exception('Error getting coils: {coils}') # trying to display coils.bits would fail
+        if not discrete_inputs.isError():
+            print("Discrete Inputs:", discrete_inputs.bits)
 
+        if not holding_registers.isError():
+            print("Holding Registers:", holding_registers.registers)
 
-# Print the values
-print("Coils:", coils.bits)
-print("Discrete Inputs:", discrete_inputs.bits)
-print("Holding Registers:", holding_registers.registers)
-print("Input Registers:", input_registers.registers)
+        if not input_registers.isError():
+            print("Input Registers:", input_registers.registers)
 
-client.close()
+        print("-" * 40)
+
+        time.sleep(2)  # poll every 2 seconds
+
+finally:
+    client.close()
